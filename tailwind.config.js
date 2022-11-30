d8709ca2ea0708ee62050e0764165d0ae18b2762
LaupWing <loc_strict@hotmail.com>
2022-11-30 16:05:39 +0100

    base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
    content: [
-     "./pages/**/*.{js,ts,jsx,tsx}",
-     "./components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
-     extend: {},
+      extend: {
+         colors:{
+            "background-primary": "#0e1217",
+            "background-secundair": "#1c1f26",
+            "accent": "#a8b3cf",
+         }
+      },
    },
    plugins: [],
- }
+}
