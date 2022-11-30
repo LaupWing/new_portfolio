deae53e7a189b304aa463587989bd3407baed9fd
LaupWing <loc_strict@hotmail.com>
2022-11-30 16:18:07 +0100

    added icons and fotns

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
       "./src/components/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
+      fontFamily: {
+         sans: ["Fira Code", "sans-serif"],
+         serif: ["VT323", "sans-serif"],
+      },
       extend: {
-         colors:{
+         colors: {
             "background-primary": "#0e1217",
             "background-secundair": "#1c1f26",
             "accent": "#a8b3cf",
-         }
+         },
       },
    },
    plugins: [],
