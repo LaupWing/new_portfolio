e13273916d2dded50eb511c9f80355913ed281dd
LaupWing <loc_strict@hotmail.com>
2022-11-30 17:37:30 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import { IoIosClose } from "react-icons/io"
 import { BiSearch } from "react-icons/bi"
 import ProjectCard from "../components/ProjectCard"
+import { IoLogoFirebase, IoLogoReact, IoLogoVue } from "react-icons/io5"
 
 export default function Home() {
    const data = [
@@ -25,8 +26,11 @@ export default function Home() {
             <p className="mb-0.5">Dear stranger,</p>
             <p>Welcome to my portfolio website. Feel free to look around. You can use the filter buttons below this message to... well to filter out projects.</p>
          </div>
-         <nav className="px-5 mx-auto max-w-7xl text-accent my-2">
+         <nav className="px-5 mx-auto max-w-7xl text-accent my-2 flex items-center space-x-4">
             <BiSearch size={30}/>
+            <IoLogoFirebase size={22}/>
+            <IoLogoReact size={22}/>
+            <IoLogoVue size={22}/>
          </nav>
          <main className="grid grid-cols-4 mb-4 gap-5 p-5 mx-auto max-w-7xl">
             {data.map(d=>(
