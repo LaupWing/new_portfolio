3b222f76fcb94e9909167da49f0adfab50b9c9fd
LaupWing <loc_strict@hotmail.com>
2022-12-01 16:29:25 +0100

    hmm

--- typings.d.ts
+++ typings.d.ts
@@ -7,6 +7,16 @@ export enum Icons {
    "css" = "IoLogoCss3",
    "typescript" = "SiTypescript"
 }
+
+export type _icons = {
+   "firebase" :  "IoLogoFirebase",  
+   "react" : "IoLogoReact" , 
+   "vue" : "IoLogoVue", 
+   "solidity" : "SiSolidity", 
+   "nextjs" : "SiNextdotjs",
+   "css" : "IoLogoCss3",
+   "typescript" : "SiTypescript"
+}
 export type skills = keyof typeof Icons
 
 export interface Skill {
