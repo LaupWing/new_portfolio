a19b5998586af80b031cf4dd59153669250102f9
LaupWing <loc_strict@hotmail.com>
2022-12-01 05:27:32 +0100

    skill

---
+++ src/components/Filters/Skill/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Skill = () => {
+   return (
+      <div className="bg-white h-10 w-10 rounded-full flex items-center justify-center gradient-animation text-white border border-accent">
+         <IoLogoFirebase size={22} />
+      </div>
+   )
+}
+
+export default Skill
