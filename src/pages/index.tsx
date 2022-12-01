de718930c176657f346873cb800a41e30080181d
LaupWing <loc_strict@hotmail.com>
2022-12-01 11:38:34 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,10 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
          </Head>
          <div className="p-5 rounded-lg max-w-xl border border-accent shadow shadow-accent text-accent mx-auto flex flex-col mt-10">
-            <IoIosClose className="ml-auto" size={30}/>
+            <IoIosClose className="ml-auto cursor-pointer hover:text-pink-500 duration-200" size={30}/>
             <p className="mb-0.5">Dear stranger,</p>
             <p>Welcome to my portfolio website. Feel free to look around. You can use the filter buttons below this message to... well to filter out projects.</p>
+            <button className="border duration-200 border-accent  ml-auto mt-2 rounded px-2 uppercase text-sm font-bold py-1 hover:bg-pink-500 hover:text-white">Dont show</button>
          </div>
          <Filters/>
          <main className="grid grid-cols-4 w-full mb-10 gap-6 px-5 mx-auto max-w-7xl">
