ac16453b1ea9d648bc7e0f7526e697103d520d70
LaupWing <loc_strict@hotmail.com>
2022-12-07 19:04:38 +0100

    update

--- src/components/ProjectCard/Overlay/index.tsx
+++ src/components/ProjectCard/Overlay/index.tsx
@@ -5,8 +5,8 @@ import { AiFillEye, AiFillGithub } from "react-icons/ai"
 const Overlay = () => {
    return (
       <div className="absolute inset-0 z-50 flex justify-center items-center flex-col space-y-10">
-         <AiFillEye size={40} className="text-white cursor-pointer"/>
-         <AiFillGithub size={40} className="text-white"/>
+         <AiFillEye size={50} className="text-white cursor-pointer hover:text-pink-500"/>
+         <AiFillGithub size={50} className="text-white cursor-pointer hover:text-pink-500"/>
       </div>
    )
 }
