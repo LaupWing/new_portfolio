2d0ef0ee89a4e34b9f00f6c72fa67c8a5e021c27
LaupWing <loc_strict@hotmail.com>
2022-12-14 19:00:27 +0100

    test

--- backend/schemas/projects.ts
+++ backend/schemas/projects.ts
@@ -4,12 +4,29 @@ export default defineType({
    name: "projects",
    title: "Projects",
    type: "document",
-   fields:[
+   fields: [
       defineField({
          name: "name",
          title: "Name",
          type: "string",
-         description: "Name of the project you are currently working on"
-      })
-   ]
+         description: "Name of the project you are currently working on",
+      }),
+      defineField({
+         name: "skills",
+         title: "Skills",
+         type: "array",
+         description: "The skills you needed to build this project",
+         of: [
+            {
+               type: "string",
+            },
+         ],
+         options: {
+            list: [
+               {title: "Sci-Fi", value: "sci-fi"},
+               {title: "Western", value: "western"},
+            ],
+         },
+      }),
+   ],
 })
