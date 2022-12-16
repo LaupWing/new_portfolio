0a5d894c0ca4f68d2810af8173c67e147325626b
LaupWing <loc_strict@hotmail.com>
2022-12-16 19:07:41 +0100

    sanityClient

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,2 +1,10 @@
+import sanityClient from "@sanity/client"
 
-export const client 
+export const client = sanityClient({
+   projectId: "",
+   dataset: "production",
+   apiVersion: "",
+   useCdn: true,
+   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+})
+
