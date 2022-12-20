8161aeda3c2b9aa447f87400d8ca4625f5129c16
LaupWing <loc_strict@hotmail.com>
2022-12-20 11:46:00 +0100

    udpate

--- src/components/ProjectCard/Skill/index.tsx
+++ src/components/ProjectCard/Skill/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Tooltip } from "react-tooltip"
 import icons from "src/utils/icons"
 import { Skills } from "typings"
 
@@ -10,9 +11,19 @@ interface Props {
 const Skill:React.FC<Props> = ({ name, index }) => {
    const Icon = (icons as any)[name]
    return (
-      <div className={"w-8 h-8 flex justify-center items-center border-2 border-accent bg-white rounded-full transform " + `-translate-x-${index * 2}`}>
-         <Icon className="text-background-secundair" size={20}/>
-      </div>
+      <>
+         <Tooltip 
+            anchorId={name} 
+            content={name} 
+            place="top"
+         />
+         <div 
+            className={"w-8 h-8 flex justify-center items-center border-2 border-accent bg-white rounded-full transform " + `-translate-x-${index * 2}`}
+            id={name}
+         >
+            <Icon className="text-background-secundair" size={20}/>
+         </div>
+      </>
    )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout"
-import { Tooltip } from "react-tooltip"
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
       <Layout>
-         <Tooltip/>
          <Component {...pageProps} />
       </Layout>
    )
