0510d561c1920949a8bfbd7402083d1926284ebb
LaupWing <loc_strict@hotmail.com>
2022-12-20 12:06:36 +0100

    toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout"
+import { ToastContainer } from "react-toastify"
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
       <Layout>
+         <ToastContainer />
          <Component {...pageProps} />
       </Layout>
    )
