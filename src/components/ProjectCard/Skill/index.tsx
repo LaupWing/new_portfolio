879594068acc7b3824b145631a248a2e436d65ae
LaupWing <loc_strict@hotmail.com>
2022-12-20 11:48:20 +0100

    wtf

--- src/components/ProjectCard/Skill/index.tsx
+++ src/components/ProjectCard/Skill/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Tooltip } from "react-tooltip"
 import icons from "src/utils/icons"
 import { Skills } from "typings"
-
+import 'react-tooltip/dist/react-tooltip.css'
 interface Props {
    name: Skills
    index: number
@@ -13,13 +13,13 @@ const Skill:React.FC<Props> = ({ name, index }) => {
    return (
       <>
          <Tooltip 
-            anchorId={name} 
-            content={name} 
+            anchorId="test" 
+            content="{name}" 
             place="top"
          />
          <div 
             className={"w-8 h-8 flex justify-center items-center border-2 border-accent bg-white rounded-full transform " + `-translate-x-${index * 2}`}
-            id={name}
+            id="test"
          >
             <Icon className="text-background-secundair" size={20}/>
          </div>
