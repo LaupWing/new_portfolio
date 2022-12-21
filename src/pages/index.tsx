1b5430b2455559eddf40787c3b1db5ba7fa4aaed
LaupWing <loc_strict@hotmail.com>
2022-12-21 16:29:28 +0100

    udpate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,7 +150,15 @@ export default function Home() {
             <IoIosClose className="ml-auto cursor-pointer hover:text-pink-500 duration-200" size={30}/>
             <p className="mb-0.5">Dear stranger,</p>
             <p>Welcome to my portfolio website. Feel free to look around. You can use the filter buttons below this message to... well to filter out projects.</p>
-            <button className="border duration-200 border-accent  ml-auto mt-2 rounded px-2 uppercase text-sm font-bold py-1 hover:bg-pink-500 hover:text-white">Dont show</button>
+            <button 
+               className={`border duration-200 border-accent  ml-auto mt-2 rounded px-2 uppercase text-sm font-bold py-1 hover:bg-pink-500 hover:text-white ${
+                  darkMode ?
+                     "" :
+                     "bg-white hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)] shadow-[2px_2px_0px_2px_rgba(0,0,0,1)]"
+               }`}
+            >
+               Dont show
+            </button>
          </div>
          <Filters
             skills={filters}
