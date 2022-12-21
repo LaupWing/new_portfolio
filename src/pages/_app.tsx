d8077719158524dde4342f872f0dd7cbb3623bae
LaupWing <loc_strict@hotmail.com>
2022-12-21 13:40:09 +0100

    its now wokring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout"
 import { ToastContainer } from "react-toastify"
-
+import {useEffect} from "react"
 export default function App({ Component, pageProps }: AppProps) {
+
+   useEffect(()=>{
+      document.documentElement.classList.add("dark")
+   },[])
    return (
       <Layout>
          <ToastContainer />
