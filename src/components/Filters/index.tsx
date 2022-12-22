d88a17e95a14660053fae6add4f8a4d2f6e0f1f2
LaupWing <loc_strict@hotmail.com>
2022-12-22 14:17:51 +0100

    moved

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify"
 import { useAppSelector } from "src/app/hooks"
 import { Skill } from "typings"
 import SkillCmp from "./Skill"
+import Skills from "./Skills"
 
 interface Props {
    skills:Skill[]
@@ -40,53 +41,14 @@ const Filters:FC<Props> = ({
          <AnimatePresence mode="wait">
             {show_search ? 
                (
-                  <motion.div 
-                     initial={{
-                        y: 100
-                     }}
-                     animate={{
-                        y: 0
-                     }}
-                     exit={{
-                        y: 100
-                     }}
-                     key={"filters"}
-                     className="flex items-center space-x-4"
-                  >
-                     <BiBomb 
-                        size={35}
-                        className={"shrink-0 " + (darkMode ? "" : "text-black")}
-                     />
-                     {skills.map(skill=>(
-                        <SkillCmp
-                           key={skill.name}
-                           name={skill.name}
-                           active={skill.active}
-                           onClick={()=>{
-                              toggle(skill.name)
-                              toast(<p>{!skill.active ? "Show": "Hide"} <b>{skill.name}</b></p>)
-                           }}
-                        />
-                     ))}
-                     <button 
-                        className={"text-sm rounded-full shrink-0 duration-200 w-20 text-center border-2 py-0.5 uppercase font-bold font-serif tracking-widest " + 
-                           (showSelf ? "bg-teal-500 text-white " : "text-teal-500 opacity-30 ") +
-                           (darkMode ? "border-teal-400" : "border-black shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]")
-                        }
-                        onClick={()=> setShowSelf(prev => !prev)}
-                     >
-                        Self
-                     </button>
-                     <button 
-                        className={"text-sm duration-200 shrink-0 rounded-full w-20 text-center py-0.5 uppercase border-2 font-bold font-serif tracking-widest " + 
-                           (showTutorial ? "bg-indigo-500 text-white " : "text-indigo-500 opacity-30 ") +
-                           (darkMode ? "border-indigo-500" : "border-black shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]")
-                        }
-                        onClick={()=> setShowTutorial(prev => !prev)}
-                     >
-                        tutorial
-                     </button>
-                  </motion.div>
+                  <Skills
+                     setShowSelf={setShowSelf}
+                     setShowTutorial={setShowTutorial}
+                     showSelf={showSelf}
+                     showTutorial={showTutorial}
+                     skills={skills}
+                     toggle={toggle}
+                  />
                ) :
                (
                   <motion.div 
