94e039720de9057935ec52cb1ac9b46f56005106
LaupWing <loc_strict@hotmail.com>
2022-12-22 14:19:59 +0100

    moved searc

---
+++ src/components/Filters/Search/index.tsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion"
+
+const Search = () => {
+   return (
+      <motion.div 
+         initial={{
+            y: -100
+         }}
+         animate={{
+            y: 0
+         }}
+         exit={{
+            y: -100,
+         }}
+         key={"search"}
+         className="border-2 flex overflow-hidden border-black rounded-md shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]"
+      >
+         <input type="text" className="flex-1 py-1 px-2" />
+      </motion.div>
+   )
+}
+
+export default Search
