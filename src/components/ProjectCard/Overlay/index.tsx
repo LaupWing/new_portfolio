519c880bd9fc81810c5b198ca30ce2f3d240e6a3
LaupWing <loc_strict@hotmail.com>
2022-12-22 13:23:44 +0100

    updte

--- src/components/ProjectCard/Overlay/index.tsx
+++ src/components/ProjectCard/Overlay/index.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { motion } from "framer-motion"
 import { AiFillEye, AiFillGithub } from "react-icons/ai"
+import { useAppSelector } from "src/app/hooks"
 
 const Overlay = () => {
+   const { darkMode } = useAppSelector(state => state.theme)
+
    return (
       <motion.div 
          className="absolute inset-0 z-50 flex justify-center items-center flex-col space-y-10"
@@ -28,7 +31,14 @@ const Overlay = () => {
                duration: 0.25,
             }}
          >
-            <AiFillEye size={50} className="text-white cursor-pointer hover:text-pink-500"/>
+            <AiFillEye 
+               size={50} 
+               className={"cursor-pointer hover:text-pink-500 " + (
+                  darkMode ? 
+                     "text-white" :
+                     "text-yellow-400"
+               )}
+            />
          </motion.div>
          <motion.div
             whileHover={{
