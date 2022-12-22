b211b564b5729b76ef0118d3e33e92dd3281847e
LaupWing <loc_strict@hotmail.com>
2022-12-22 14:25:26 +0100

    update

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,9 +1,11 @@
 import { FC, useState } from "react"
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence } from "framer-motion"
 import { BiSearch } from "react-icons/bi"
 import { useAppSelector } from "src/app/hooks"
 import { Skill } from "typings"
 import Skills from "./Skills"
+import Search from "./Search"
+import { motion } from "framer-motion"
 
 interface Props {
    skills:Skill[]
@@ -26,16 +28,54 @@ const Filters:FC<Props> = ({
    const [show_search, setShowSearch] = useState(false)
 
    return (
-      <nav className={"px-5 h-20 max-w-7xl w-full mx-auto text-accent flex items-center space-x-4 justify-start shrink-0 overflow-x-auto duration-200 sticky top-0 z-50 " + (
+      <nav className={"px-5 h-20 max-w-7xl w-full mx-auto text-accent flex items-center space-x-4 justify-start shrink-0 overflow-x-auto duration-200 sticky overflow-hidden top-0 z-50 " + (
          darkMode ? 
             "bg-background-primary" :
             "bg-white"
       )}>
-         <BiSearch
-            onClick={() => setShowSearch(!show_search)}
-            size={35}
-            className={"cursor-pointer hover:text-yellow-400 duration-200 shrink-0 " + (darkMode ? "" : "text-black")}
-         />
+         <AnimatePresence mode="wait">
+            {show_search ?
+               (
+                  <motion.div
+                     initial={{
+                        y: -60
+                     }}
+                     animate={{
+                        y: 0
+                     }}
+                     exit={{
+                        y: -60,
+                     }}
+                     key={"search-btn"}
+                  >
+                     <BiSearch
+                        onClick={() => setShowSearch(!show_search)}
+                        size={35}
+                        className={"cursor-pointer hover:text-yellow-400 duration-200 shrink-0 " + (darkMode ? "" : "text-black")}
+                     />
+                  </motion.div>
+               ) : (
+                  <motion.div
+                     initial={{
+                        x: -60
+                     }}
+                     animate={{
+                        x: 0
+                     }}
+                     exit={{
+                        x: -60,
+                     }}
+                     key={"close-btn"}
+                  >
+                     <BiSearch
+                        onClick={() => setShowSearch(!show_search)}
+                        size={35}
+                        className={"cursor-pointer hover:text-yellow-400 duration-200 shrink-0 " + (darkMode ? "" : "text-black")}
+                     />
+                  </motion.div>
+               )
+            }
+         </AnimatePresence>
          <AnimatePresence mode="wait">
             {show_search ? 
                (
@@ -49,21 +89,7 @@ const Filters:FC<Props> = ({
                   />
                ) :
                (
-                  <motion.div 
-                     initial={{
-                        y: -100
-                     }}
-                     animate={{
-                        y: 0
-                     }}
-                     exit={{
-                        y: -100,
-                     }}
-                     key={"search"}
-                     className="border-2 flex overflow-hidden border-black rounded-md shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]"
-                  >
-                     <input type="text" className="flex-1 py-1 px-2" />
-                  </motion.div>
+                  <Search/>
                )
             }
          </AnimatePresence>
