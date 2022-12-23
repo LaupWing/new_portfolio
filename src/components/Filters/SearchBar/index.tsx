d7c42c18d29cd458aaa9186c588f777529ef8158
LaupWing <loc_strict@hotmail.com>
2022-12-23 13:26:32 +0100

    udpate

--- src/components/Filters/SearchBar/index.tsx
+++ src/components/Filters/SearchBar/index.tsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion"
+import { useAppSelector } from "src/app/hooks"
 
 const Search = () => {
+   const { darkMode } = useAppSelector(state => state.theme)
+
    return (
       <motion.div 
          initial={{
@@ -13,9 +16,18 @@ const Search = () => {
             y: -100,
          }}
          key={"search"}
-         className="border-2 w-full max-w-sm flex overflow-hidden border-accent rounded-md shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]"
+         className={
+            "border-2 w-full max-w-sm flex overflow-hidden  rounded-md " +
+            (darkMode ? 
+               "border-accent" :
+               "border-black shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]"   
+            )
+         }
       >
-         <input type="text" className="flex-1 py-1 px-2" />
+         <input 
+            type="text" 
+            className="flex-1 py-1 px-2 focus:outline-none" 
+         />
       </motion.div>
    )
 }
