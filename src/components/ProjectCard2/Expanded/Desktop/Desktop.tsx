c69833704bb2d4abefb7c60ef4faa18b4f2f80e9
LaupWing <loc_strict@hotmail.com>
2022-12-23 14:47:03 +0100

    base

--- src/components/ProjectCard2/Expanded/Desktop/Desktop.tsx
+++ src/components/ProjectCard2/Expanded/Desktop/Desktop.tsx
@@ -2,17 +2,19 @@ import React from "react"
 
 const Desktop = ({image}: any) => {
    return (
-      <div>
-         <h1 className="font-serif text-5xl tracking-wider">ProjectCard</h1>
-         <p className="text-lg mt-1 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet sit
-            doloribus ab ipsa maiores ad dolorum.
-         </p>
+      <div className="hidden md:flex">
          <img
             src={image}
-            className="aspect-video w-full object-cover rounded mt-4"
+            className="object-cover w-1/2 rounded mt-4"
             alt="Project image"
          />
+         <div className="flex flex-col px-4 mt-2">
+            <h1 className="font-serif text-5xl tracking-wider">ProjectCard</h1>
+            <p className="text-lg mt-1 line-clamp-3">
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet sit
+               doloribus ab ipsa maiores ad dolorum.
+            </p>
+         </div>
       </div>
    )
 }
--- src/components/ProjectCard2/Expanded/Mobile/index.tsx
+++ src/components/ProjectCard2/Expanded/Mobile/index.tsx
@@ -2,17 +2,17 @@ import React from "react"
 
 const Mobile = ({image}: any) => {
    return (
-      <div className="flex flex-col">
-         <h1 className="font-serif text-5xl tracking-wider">ProjectCard</h1>
-         <p className="text-lg mt-1 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet sit
-            doloribus ab ipsa maiores ad dolorum.
-         </p>
+      <div className="md:hidden flex flex-col">
          <img
             src={image}
             className="aspect-video w-full object-cover rounded mt-4"
             alt="Project image"
          />
+         <h1 className="font-serif text-5xl tracking-wider">ProjectCard</h1>
+         <p className="text-lg mt-1 line-clamp-3">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet sit
+            doloribus ab ipsa maiores ad dolorum.
+         </p>
       </div>
    )
 }
