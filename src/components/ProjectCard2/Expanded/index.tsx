d88c6c8b3b8ddabd4b90e510d6fb6546b5063d37
LaupWing <loc_strict@hotmail.com>
2022-12-23 14:30:09 +0100

    some base stylingf

--- src/components/ProjectCard2/Expanded/index.tsx
+++ src/components/ProjectCard2/Expanded/index.tsx
@@ -18,7 +18,7 @@ const Expanded:React.FC<Props> = ({ skills, tutorial, image }) => {
    return (
       <motion.div 
          layoutId="expandable-card"
-         className={`overflow-hidden fixed w-screen h-screen inset-0 duration-200 z-50 p-4 rounded-2xl ${
+         className={`overflow-hidden fixed w-screen h-screen inset-0 duration-200 z-50 p-4 ${
             darkMode ? 
                "bg-background-secundair border-accent border text-accent" : 
                "bg-emerald-300 border-2 border-black hover:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)] shadow-[10px_10px_0px_2px_rgba(0,0,0,1)]" 
@@ -31,7 +31,7 @@ const Expanded:React.FC<Props> = ({ skills, tutorial, image }) => {
                paddingBottom: "115%",
             }}
          >
-            <div className={"absolute inset-0 duration-500 flex flex-col justify-between " + (hover ? "blur" : "" )}>
+            <div className="absolute inset-0 duration-500 flex flex-col">
                <div className="flex items-start justify-between">
                   <div className="flex">
                      {skills.map((s, i) => (
@@ -45,7 +45,7 @@ const Expanded:React.FC<Props> = ({ skills, tutorial, image }) => {
                   {  tutorial 
                      ? (
                         <p className={
-                           "text-sm bg-indigo-500 text-white rounded-full w-20 text-center py-0.5 uppercase font-bold font-serif tracking-widest " + 
+                           "text-base bg-indigo-500 text-white rounded-full w-24 text-center py-0.5 uppercase font-bold font-serif tracking-widest " + 
                            (darkMode ? "" : "border-2 border-black")
                         }>
                            tutorial
@@ -61,10 +61,10 @@ const Expanded:React.FC<Props> = ({ skills, tutorial, image }) => {
                         </p>
                      )}
                </div>
-               <h1 className="font-serif text-xl tracking-wider">
+               <h1 className="font-serif text-5xl tracking-wider">
                   ProjectCard
                </h1>
-               <p className="text-xs mt-1 line-clamp-3">
+               <p className="text-lg mt-1 line-clamp-3">
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
                   sit doloribus ab ipsa maiores ad dolorum.
                </p>
