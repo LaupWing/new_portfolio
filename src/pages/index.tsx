c26fa73b10f9a850a9357ad5260650a85f21b0c0
LaupWing <loc_strict@hotmail.com>
2022-12-26 14:36:10 +0100

    it works now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css"
 import Message from "src/components/Message"
 import { GetServerSideProps } from "next"
 import { client } from "src/sanity"
-import bomb_sound from "../sounds/bomb.mp3"
+// import bomb_sound from "/sounds/bomb.mp3"
 import useSound from "use-sound"
 
 const mainContainerVariants: Variants = {
@@ -66,7 +66,9 @@ export default function Home({projects}:any) {
    ])
    const [showSelf, setShowSelf] = useState(true) 
    const [showTutorial, setShowTutorial] = useState(true)
-   const [] = useSound(bomb_sound)
+   const [play] = useSound("/assets/sounds/bomb.mp3", {
+      volume: 0.2
+   })
    
    const toggleAll = () =>{
       const extra_filters = 2
@@ -90,7 +92,7 @@ export default function Home({projects}:any) {
       }else{
          setAll(true)
       }
-
+      play()
    } 
 
    const active_skills = filters
