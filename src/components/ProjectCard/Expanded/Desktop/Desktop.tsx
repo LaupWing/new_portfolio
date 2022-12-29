1340786f5aa9f1782d2d43967dc4e5785b935f42
LaupWing <loc_strict@hotmail.com>
2022-12-29 16:57:26 +0100

    fixed message

--- src/components/ProjectCard/Expanded/Desktop/Desktop.tsx
+++ src/components/ProjectCard/Expanded/Desktop/Desktop.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 const Desktop:React.FC<Props> = ({project}) => {
-   console.log(project)
    return (
       <div className="hidden md:flex">
          <Images
@@ -19,9 +18,25 @@ const Desktop:React.FC<Props> = ({project}) => {
                github={project.github}
                url={project.url}
             />
-            {project.credentials && <p>
-
-            </p>}
+            {project.credentials && 
+               <div className="flex flex-col  my-4">
+                  <p className="text-slate-600 font-bold uppercase font-serif tracking-widest mb-2">⚠️⚠️ In this app you need to login!! 👇</p>
+                  <table className="table-auto bg-slate-100 border-2 shadow border-slate-300 rounded mr-auto">
+                     <thead className="bg-slate-300 text-slate-400">
+                        <tr className="divide-x-2">
+                           <th className="px-4 py-2">Username</th>
+                           <th className="px-4 py-2">Password</th>
+                        </tr>
+                     </thead>
+                     <tbody className="text-slate-700">
+                        <tr className="divide-x-2">
+                           <th className="px-4 py-2">{project.username}</th>
+                           <th className="px-4 py-2">{project.password}</th>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>
+            }
             <h1 className="font-serif text-5xl tracking-wider">{project.name}</h1>
             <p className="text-lg mt-1">
                {project.description}
