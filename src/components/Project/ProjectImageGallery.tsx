c35ebc00b14c268b2b54c7b0d39548f77bf7c826
LaupWing <loc_strict@hotmail.com>
2023-02-12 17:02:47 +0100

    update

---
+++ src/components/Project/ProjectImageGallery.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ImageGallery from "react-image-gallery"
+import { urlFor } from "src/sanity"
+
+interface Props {
+   images: string[]
+}
+
+export const ProjectImageGallery:React.FC<Props> = ({images}) => {
+   const _images = images
+      .map((img) => urlFor(img).url())
+      .map((x:string)=>({
+         original: x,
+         thumbnail: x,
+      }))
+   return (
+      <div className="md:w-1/2 md:mb-0 mb-2 w-full flex-shrink-0">
+         <ImageGallery items={_images} />
+      </div>
+   )
+}
