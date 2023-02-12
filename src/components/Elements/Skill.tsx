67f384b7b66c15236cbc952b6f0ba11e695b564e
LaupWing <loc_strict@hotmail.com>
2023-02-12 16:39:33 +0100

    SKill

---
+++ src/components/Elements/Skill.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react"
+import { useAppSelector } from "src/app/hooks"
+import icons from "src/utils/skills"
+
+interface Props {
+   name: string,
+   active: boolean,
+   onClick: () => void
+}
+
+const Skill:FC<Props> = ({ name, active, onClick }) => {
+   const Icon = (icons as any)[name]
+   const {darkMode} = useAppSelector(state => state.theme)
+   
+   return (
+      <div className="relative">
+         <button 
+            className={"h-10 w-10 rounded-full shrink-0 cursor-pointer duration-200 flex items-center justify-center " + 
+               (active ? "gradient-animation " : "opacity-30 ") + 
+               (!active && !darkMode ? "text-black " : "text-white ") +
+               (darkMode ? 
+                  "border border-accent" : 
+                  "border-2 border-black shadow-[2px_2px_0px_1px_rgba(0,0,0,1)]")
+            }
+            onClick={onClick}
+            id="test"
+         >
+            <Icon size={22}/>
+         </button>
+      </div>
+   )
+}
+
+export default Skill
