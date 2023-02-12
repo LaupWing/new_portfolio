60d3e533867ca5552b6935940963a631150314f0
LaupWing <loc_strict@hotmail.com>
2023-02-12 17:11:43 +0100

    udpate

--- src/components/Cards/ProjectCard.tsx
+++ src/components/Cards/ProjectCard.tsx
@@ -2,9 +2,9 @@ import { Variants, motion } from "framer-motion"
 import React, { useState } from "react"
 import { useAppSelector } from "src/app/hooks"
 import { urlFor } from "src/sanity"
-import { Project } from "typings"
-import Overlay from "./Overlay"
-import Skill from "./Skill"
+import { Project, Skills } from "typings"
+import icons from "src/utils/skills"
+import { AiFillEye, AiFillGithub } from "react-icons/ai"
 
 interface Props {
    project: Project
@@ -99,3 +99,101 @@ const Normal:React.FC<Props> = ({ project, setExpanded }) => {
 }
 
 export default Normal
+
+interface SkillProps {
+   name: Skills
+   index: number
+}
+
+const Skill:React.FC<SkillProps> = ({ name, index }) => {
+   const Icon = (icons as any)[name]
+   const {darkMode} = useAppSelector(state => state.theme)
+   return (
+      <div 
+         className={"w-8 h-8 flex justify-center items-center border-2 bg-white rounded-full transform " + 
+            `-translate-x-${index * 2} ` +
+            (darkMode ? "border-accent" : "border-black")
+         }
+         id="test"
+      >
+         <Icon className="text-background-secundair" size={20}/>
+      </div>
+   )
+}
+
+
+const Overlay = () => {
+   const { darkMode } = useAppSelector(state => state.theme)
+
+   return (
+      <motion.div 
+         className="absolute cursor-pointer inset-0 z-50 flex justify-center items-center flex-col space-y-10"
+         initial={{
+            scale: 1.5,
+            opacity: 0
+         }}
+         animate={{
+            scale: 1,
+            opacity: 1
+         }}
+         transition={{
+            delay: 0.2,
+            type: "spring",
+            stiffness: 120
+         }}
+      >
+         <motion.p
+            className={"absolute bg-pink-100 px-2 rounded bg-opacity-70 top-4 text-center font-bold uppercase " + (
+               darkMode ?
+                  "text-violet-500" : 
+                  "text-pink-400"
+            )}
+            initial={{
+               y: -20,
+            }}
+            animate={{
+               y: 0,
+            }}
+            transition={{
+               delay: 0.2
+            }}
+         >
+            Click me to learn more 🖱️ 
+         </motion.p>
+         <motion.div
+            whileHover={{
+               scale: [1, 0.85],
+            }}
+            transition={{
+               duration: 0.25,
+            }}
+         >
+            <AiFillEye
+               size={50} 
+               className={"cursor-pointer hover:text-pink-500 " + (
+                  darkMode ? 
+                     "text-indigo-500" :
+                     "text-yellow-400"
+               )}
+            />
+         </motion.div>
+         <motion.div
+            whileHover={{
+               scale: [1, 0.85],
+            }}
+            transition={{
+               duration: 0.25,
+            }}
+         >
+            <AiFillGithub 
+               size={50} 
+               className={"cursor-pointer hover:text-pink-500 " + (
+                  darkMode ? 
+                     "text-indigo-500" :
+                     "text-yellow-400"
+               )}
+            />
+         </motion.div>
+      </motion.div>
+   )
+}
