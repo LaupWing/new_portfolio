908ca6d5a7ff1602b44447b7614622400d99e571
LaupWing <loc_strict@hotmail.com>
2023-02-12 17:33:24 +0100

    udpate

--- src/components/Elements/ProjectLabel.tsx
+++ src/components/Elements/ProjectLabel.tsx
@@ -1,4 +1,19 @@
+import { useAppSelector } from "src/app/hooks"
+
 const ProjectLabel = () => {
-   return <div>ProjectLabel</div>
+   const {darkMode} = useAppSelector(state => state.theme)
+   
+   return (
+      <p 
+         className={
+         "text-sm text-white rounded-full w-20 text-center py-0.5 uppercase font-bold font-serif tracking-widest " + 
+            (darkMode 
+               ? "" 
+               : "border-2 border-black"
+            )}
+      >
+         ProjectLabel
+      </p>
+   )
 }
 export default ProjectLabel
