bcf6d5a77008b5ea359734b16e9595e45be5f4d8
LaupWing <loc_strict@hotmail.com>
2023-02-12 17:35:56 +0100

    update

--- src/components/Elements/ProjectLabel.tsx
+++ src/components/Elements/ProjectLabel.tsx
@@ -1,18 +1,26 @@
+import clsx from "clsx"
+import { FC } from "react"
 import { useAppSelector } from "src/app/hooks"
 
-const ProjectLabel = () => {
+interface ProjectLabelProps {
+   label: string
+   className?: string
+}
+
+const ProjectLabel:FC<ProjectLabelProps> = ({
+   label,
+   className = ""
+}) => {
    const {darkMode} = useAppSelector(state => state.theme)
-   
+   const styles = clsx(
+      "text-sm text-white rounded-full w-20 text-center py-0.5 uppercase font-bold font-serif tracking-widest",
+      className,
+      darkMode && "border-2 border-black"
+   )
+
    return (
-      <p 
-         className={
-         "text-sm text-white rounded-full w-20 text-center py-0.5 uppercase font-bold font-serif tracking-widest " + 
-            (darkMode 
-               ? "" 
-               : "border-2 border-black"
-            )}
-      >
-         ProjectLabel
+      <p className={styles}>
+         {label}
       </p>
    )
 }
