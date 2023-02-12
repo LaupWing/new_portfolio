1b4e61513c8d8a5910caf8b1ab1aaf90fc033cdd
LaupWing <loc_strict@hotmail.com>
2023-02-12 16:52:57 +0100

    udpate

--- src/components/Sections/index.ts
+++ src/components/Sections/index.ts
@@ -1,1 +1,2 @@
 export { Filters } from "./Filters"
+export { Message } from "./Message"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
 import "react-image-gallery/styles/css/image-gallery.css"
+import "react-toastify/dist/ReactToastify.css"
 import { ToastContainer } from "react-toastify"
 import { Provider } from "react-redux"
 import { store } from "src/app/store"
