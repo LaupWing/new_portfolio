b2a5b479cda06807854766461c410eacf69926f2
LaupWing <loc_strict@hotmail.com>
2023-02-17 16:13:04 +0100

    fetch

--- src/pages/projects/[project].tsx
+++ src/pages/projects/[project].tsx
@@ -1,4 +1,20 @@
+import { GetStaticProps } from "next"
+import { client } from "src/sanity"
+
 const ProjectDetail = () => {
    return <div>ProjectDetail</div>
 }
 export default ProjectDetail
+
+export const getStaticProps: GetStaticProps = async (context) => {
+   const projectSlug = context.params?.product
+   const projectQuery = "*[_type == 'project' && slug.current == $projectSlug]"
+   const projectDetail = await client.fetch(projectQuery, {
+      projectSlug
+   })
+   console.log(ProjectDetail)
+
+   return {
+      props: {}
+   }
+}
