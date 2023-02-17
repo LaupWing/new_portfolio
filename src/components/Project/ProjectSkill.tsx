9f5ed1e015e8e10490b1be6bc3013eb7966b2988
LaupWing <loc_strict@hotmail.com>
2023-02-17 18:18:11 +0100

    fast tracked

--- src/components/Project/ProjectSkill.tsx
+++ src/components/Project/ProjectSkill.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react"
+import { useAppSelector } from "src/app/hooks"
 import { Skills } from "typings"
+import icons from "src/utils/skills"
 
 interface ProjectSkillProps {
    name: Skills
@@ -10,6 +12,19 @@ const ProjectSkill:FC<ProjectSkillProps> = ({
    name,
    index
 }) => {
-   return <div>ProjectSkill</div>
+   const Icon = (icons as any)[name]
+   const {darkMode} = useAppSelector(state => state.theme)
+
+   return (
+      <div 
+         className={"w-12 h-12 flex justify-center items-center border-2 bg-white rounded-full transform " + 
+            `-translate-x-${index * 2} ` +
+            (darkMode ? "border-accent" : "border-black")
+         }
+         id="test"
+      >
+         <Icon className="text-background-secundair" size={28}/>
+      </div>
+   )
 }
 export default ProjectSkill
